a87c276ec1810c50b649df80cdb156716028323d
CarlaN100 <carla.naujoks@freenet.de>
2016-10-25 16:39:43 +0200

    new exercises

---
+++ EX11/ex11_a.py
@@ -0,0 +1,7 @@
+#Hier ist es so, dass wenn du das Komma weg lässt, die Antwort in die nöchste Spalte gedruckt wird. 
+print "What is your Hobby",
+Hobby = raw_input()
+print "What is your favorite food?",
+Food = raw_input()
+print "What is your Job?"
+Job = raw_input()
---
+++ INCLASS/inclass_argv.py
@@ -0,0 +1,11 @@
+from sys import argv
+
+print argv
+
+#This is multiple assignment
+arg1, arg2, arg3 = argv
+
+#it is identical functionally to
+arg1 = argv[0]
+arg2 = argv[1]
+arg3 = argv[2]
